e70b21e347d47588beb7fff8ee54af21f31d9389
Xavier Au <xavier.au@anacreation.com>
2017-02-10 15:37:44 +0800

    test completed for packagist

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -13,7 +13,6 @@ class MediaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //for testing
         $this->loadMigrationsFrom(__DIR__.'/Migration');
     }
 
